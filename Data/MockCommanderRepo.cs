5642d4fe24ec8c5f498cffaf9c4ae03c60eac655
purch1 <ucheprince26@gmail.com>
2025-07-08 15:38:07 +0100

    Initial commit: ASP.NET Core Commander API with mock data repository

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan" },
+                new Command{Id=1, HowTo="Fish and Milk", Line="Boil fish", Platform="Laptop" },
+                new Command{Id=2, HowTo="Rice and beans", Line="Monitor", Platform="Table is clean" },
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan" };
+        }
+    }
+}

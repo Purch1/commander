ec9ef07d4b1ad8219fd6f625fc5eddddbb1c6094
purch1 <ucheprince26@gmail.com>
2025-07-09 14:46:52 +0100

    Refactor CommandsController to use CommandReadDto for response types and update CommandReadDto to comment out unused Platform property

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,30 +13,31 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
 
-        public CommandsController(ICommanderRepo repository)
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
         //Get api/commands
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandItems = _repository.GetAllCommands();
-
-            return Ok(commandItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
         //Get api/commands/{id}
         [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandsById(int id) 
+        public ActionResult <CommandReadDto> GetCommandsById(int id) 
         {
             var commandItem = _repository.GetCommandById(id);
             if (commandItem == null)
             {
                 return NotFound();
             }
-            return Ok(commandItem);
+            return Ok(_mapper.Map<CommandReadDto>(commandItem));
         }
     }
 }

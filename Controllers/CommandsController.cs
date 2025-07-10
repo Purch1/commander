1b2c7aef0def16fcec6beaa62a8cb72ccf0bb050
purch1 <ucheprince26@gmail.com>
2025-07-10 01:38:47 +0100

    Remove unused project files and update project configurations; refactor CommandsController namespace and adjust API routes

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -6,9 +6,8 @@ using Commander.Domain.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Commander.Controllers
+namespace Commander.API.Controllers
 {
-
     [Route("api/commands")]
     [ApiController]
     public class CommandsController : ControllerBase
@@ -22,61 +21,68 @@ namespace Commander.Controllers
             _mapper = mapper;
         }
 
-
-        //Get api/commands
+        //GET api/commands
         [HttpGet]
         public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandItems = _repository.GetAllCommands();
+
             return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
-        //Get api/commands/{id}
-        [HttpGet("{id}", Name = "GetCommandsById")]
-        public ActionResult<CommandReadDto> GetCommandsById(int id)
+        //GET api/commands/{id}
+        [HttpGet("{id}", Name = "GetCommandById")]
+        public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
-            if (commandItem == null)
+
+            if (commandItem != null)
             {
-                return NotFound();
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
             }
-            return Ok(_mapper.Map<CommandReadDto>(commandItem));
+
+            return NotFound();
         }
 
-        //Post api/commands
+        //POST api/commands
         [HttpPost]
-        public ActionResult<CommandReadDto> CreateCommand(CommandUpdateDto commandCreateDto)
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
         {
             var commandModel = _mapper.Map<Command>(commandCreateDto);
             _repository.CreateCommand(commandModel);
             _repository.SaveChanges();
 
             var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
-            return CreatedAtRoute(nameof(GetCommandsById), new { id = commandReadDto.Id }, commandReadDto);
+
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
         }
 
-        //Put api/commands/{id}
+        //PUT api/commands/{id}
         [HttpPut("{id}")]
         public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
         {
             var commandModelFromRepo = _repository.GetCommandById(id);
+
             if (commandModelFromRepo == null)
             {
                 return NotFound();
             }
 
             _mapper.Map(commandUpdateDto, commandModelFromRepo);
+
             _repository.UpdateCommand(commandModelFromRepo);
+
             _repository.SaveChanges();
 
             return NoContent();
         }
 
-        // Patch api/commands/{id}
+        //PATCH api/commands/{id}
         [HttpPatch("{id}")]
         public ActionResult PartialCommandUpdate(int id, JsonPatchDocument<CommandUpdateDto> patchDoc)
         {
             var commandModelFromRepo = _repository.GetCommandById(id);
+
             if (commandModelFromRepo == null)
             {
                 return NotFound();
@@ -91,17 +97,20 @@ namespace Commander.Controllers
             }
 
             _mapper.Map(commandToPatch, commandModelFromRepo);
+
             _repository.UpdateCommand(commandModelFromRepo);
+
             _repository.SaveChanges();
 
             return NoContent();
         }
 
-        //Delete api/commands/{id}
+        //DELETE api/commands/{id}
         [HttpDelete("{id}")]
         public ActionResult DeleteCommand(int id)
         {
             var commandModelFromRepo = _repository.GetCommandById(id);
+
             if (commandModelFromRepo == null)
             {
                 return NotFound();
@@ -112,6 +121,5 @@ namespace Commander.Controllers
 
             return NoContent();
         }
-
     }
 }
